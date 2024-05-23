d2cea35cd372f409b6726aa701c62baa5622f46c
jicodes <liu.ji1@northeastern.edu>
2024-05-23 16:48:30 -0700

    add solution to leetcode 189

---
+++ 01-array-string/189_rotate_array.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n  # Ensure k is within the range [0, n)
+        
+        # Handle left rotations
+        if k > 0:
+            i = n - k
+            nums[:] = nums[i:] + nums[:i]
