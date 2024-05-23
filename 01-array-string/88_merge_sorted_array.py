0a8646f43a1f6073ab2cb45c20e00e0bab7a9785
jicodes <liu.ji1@northeastern.edu>
2024-05-23 16:43:08 -0700

    add solution to leetcode 27

--- 01-array-string/88_merge_sorted_array.py
+++ 01-array-string/88_merge_sorted_array.py
@@ -1,10 +1,11 @@
 from typing import List
 
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         # Start from the end of nums1 and nums2
         i, j, k = m - 1, n - 1, m + n - 1
-        
+
         # Merge in reverse order
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
@@ -14,7 +15,7 @@ class Solution:
                 nums1[k] = nums2[j]
                 j -= 1
             k -= 1
-        
+
         # If nums2 is not yet exhausted
         while j >= 0:
             nums1[k] = nums2[j]
