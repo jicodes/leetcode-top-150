c0d42a94afd53e37b3ed56ecfb97a360c7d5fe1f
jicodes <liu.ji1@northeastern.edu>
2024-06-17 21:47:35 -0700

    improved solution to leetcode 76

--- 03-sliding-window/76_minimum_window_substring.py
+++ 03-sliding-window/76_minimum_window_substring.py
@@ -6,11 +6,14 @@ class Solution:
         if not s or not t:
             return ""
 
-        dict_t = Counter(t)
+        # Frequency count of characters in t
+        char_count_t = Counter(t)
         window_counts = defaultdict(int)
 
-        required = len(dict_t)
-        formed = 0
+        # Number of unique characters in t that need to be present in the window with required frequency
+        total_required_chars = len(char_count_t)
+        # Number of unique characters in the current window that match the required frequency in t
+        valid_char_count = 0
 
         left, right = 0, 0
         min_len = float("inf")
@@ -20,19 +23,23 @@ class Solution:
             char = s[right]
             window_counts[char] += 1
 
-            if char in dict_t and window_counts[char] == dict_t[char]:
-                formed += 1
+            # Check if the current character added satisfies the frequency required
+            if char in char_count_t and window_counts[char] == char_count_t[char]:
+                valid_char_count += 1
 
-            while left <= right and formed == required:
+            # Try and contract the window till the point where it ceases to be 'valid'
+            while left <= right and valid_char_count == total_required_chars:
                 char = s[left]
 
+                # Save the smallest window until now
                 if right - left + 1 < min_len:
                     min_len = right - left + 1
                     min_left = left
 
+                # The character at the position pointed by the `left` pointer is no longer a part of the window
                 window_counts[char] -= 1
-                if char in dict_t and window_counts[char] < dict_t[char]:
-                    formed -= 1
+                if char in char_count_t and window_counts[char] < char_count_t[char]:
+                    valid_char_count -= 1
 
                 left += 1
 
