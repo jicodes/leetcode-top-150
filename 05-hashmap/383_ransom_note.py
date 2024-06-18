38641e114dcf328be6d2fd12d11c3e4ccb7c9004
jicodes <liu.ji1@northeastern.edu>
2024-06-18 14:34:04 -0700

    add solution to leetcode 383

---
+++ 05-hashmap/383_ransom_note.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for char in ransomNote:
+            if char not in magazine:
+                return False
+            # replacing the first occurrence of the string char 
+            # in the string magazine with an empty string "".
+            magazine = magazine.replace(char, "", 1)
+        return True
