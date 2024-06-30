c7ecd4af2d36bb0481a48cb95085f570a178117c
jicodes <liu.ji1@northeastern.edu>
2024-06-30 10:42:25 -0700

    optimize solution to leetcode 105

--- 09-binary-tree-general/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ 09-binary-tree-general/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -1,3 +1,6 @@
+from typing import List, Optional
+
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -6,19 +9,20 @@ class TreeNode:
         self.right = right
 
 
-def buildTree(preorder, inorder):
-    if not preorder or not inorder:
-        return None
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
 
-    # The first element in preorder is the root of the tree
-    root_val = preorder[0]
-    root = TreeNode(root_val)
+        # The first element in preorder is the root of the tree
+        root_val = preorder.pop(0)
+        root = TreeNode(root_val)
 
-    # Find the root in inorder list to separate left and right subtrees
-    mid = inorder.index(root_val)
+        # Find the root in inorder list to separate left and right subtrees
+        mid = inorder.index(root_val)
 
-    # Recursively build the left and right subtrees
-    root.left = buildTree(preorder[1 : mid + 1], inorder[:mid])
-    root.right = buildTree(preorder[mid + 1 :], inorder[mid + 1 :])
+        # Recursively build the left and right subtrees
+        root.left = self.buildTree(preorder[:mid], inorder[:mid])
+        root.right = self.buildTree(preorder[mid:], inorder[mid + 1 :])
 
-    return root
+        return root
